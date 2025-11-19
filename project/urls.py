28709211d688c905920398244eeb4277935a71f2
jasvant0020 <jasvant0singh123@gmail.com>
2025-11-19 18:17:52 +0530

    munsi and admin page only frontend no backend logic

--- project/urls.py
+++ project/urls.py
@@ -24,4 +24,9 @@ urlpatterns = [
     path('police/', views.police_list, name='police_list'),
     path('vvip/', views.vvip_list, name='vvip_list'),
     path('duty/assign/', views.assign_duty, name='assign_duty'),
+     # Custom Admin Panel Pages (your dashboard, users, categories)
+    path('admin-panel/dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('admin-panel/manage-users/', views.manage_users, name='manage_users'),
+    path('admin-panel/manage-police-categories/', views.manage_police_categories, name='manage_police_categories'),
+    path('admin-panel/manage-vvip-categories/', views.manage_vvip_categories, name='manage_vvip_categories'),
 ]
