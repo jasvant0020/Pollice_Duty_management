2c3869546faf5bf6b6a745bc30b96123644ad1cb
jasvant0020 <jasvant0singh123@gmail.com>
2025-11-24 17:38:59 +0530

    new updates

--- project/urls.py
+++ project/urls.py
@@ -4,15 +4,17 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # GD Munsi Panel Pages
     path('', views.dashboard, name='dashboard'),
     path('police/', views.police_list, name='police_list'),
     path('vvip/', views.vvip_list, name='vvip_list'),
     path('duty/assign/', views.assign_duty, name='assign_duty'),
-     # Custom Admin Panel Pages (your dashboard, users, categories)
+     # Custom Admin Panel Pages
     path('admin-panel/dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('admin-panel/manage-users/', views.manage_users, name='manage_users'),
     path('admin-panel/manage-police-categories/', views.manage_police_categories, name='manage_police_categories'),
     path('admin-panel/manage-vvip-categories/', views.manage_vvip_categories, name='manage_vvip_categories'),
+    # Custom user Panel Pages 
     path("dashboard/", views.user_dashboard, name="user_dashboard"),
     path("user_dashboard/", views.user_base, name="user_base"),
     path("user_assign_duty/", views.user_assign_duty, name="user_assign_duty"),
