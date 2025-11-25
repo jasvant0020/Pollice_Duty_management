d4fdc6187dd6a9b5c130c6b08f9f9339f67be80d
jasvant0020 <jasvant0singh123@gmail.com>
2025-11-25 11:54:23 +0530

    refine code structure

--- app/views.py
+++ app/views.py
@@ -22,7 +22,7 @@ VVIP_PERSONS = [
     {'name': 'CM Yogi', 'category': 'Medium', 'location': 'National Park'},
 ]
 
-#------Custom GD Munsi Panel Views------
+#------ Custom GD Munsi Panel Views ------
 def dashboard(request):
     context = {
         'role': 'Munsi',  # change to 'Admin' to test admin view
@@ -30,17 +30,17 @@ def dashboard(request):
         'vvip_count': len(VVIP_PERSONS),
         'assignments_today': 2
     }
-    return render(request, 'dashboard.html', context)
+    return render(request, 'GD_munsi_panel/dashboard.html', context)
 
 
 def police_list(request):
     context = {'police_personnel': POLICE_PERSONNEL}
-    return render(request, 'police_list.html', context)
+    return render(request, 'GD_munsi_panel/police_list.html', context)
 
 
 def vvip_list(request):
     context = {'vvip_persons': VVIP_PERSONS}
-    return render(request, 'vvip_list.html', context)
+    return render(request, 'GD_munsi_panel/vvip_list.html', context)
 
 
 def assign_duty(request):
@@ -48,11 +48,11 @@ def assign_duty(request):
         'police_personnel': POLICE_PERSONNEL,
         'vvip_persons': VVIP_PERSONS
     }
-    return render(request, 'assign_duty.html', context)
+    return render(request, 'GD_munsi_panel/assign_duty.html', context)
 
 from django.shortcuts import render
 
-#------Custom Admin Panel Views------
+#------ Custom Admin Panel Views ------
 def admin_base(request):
     return render(request, "admin_panel/admin_base.html")
 def admin_dashboard(request):
@@ -68,72 +68,8 @@ def manage_police_categories(request):
 def manage_vvip_categories(request):
     return render(request, "admin_panel/manage_vvip_categories.html")
 
-# def manage_users(request):
-#     return render(request, "admin_panel/manage_users.html")
 
-# def manage_police_categories(request):
-#     return render(request, "admin_panel/manage_police_categories.html")
-
-# def manage_vvip_categories(request):
-#     return render(request, "admin_panel/manage_vvip_categories.html")
-
-
-# def user_dashboard(request):
-#         # ----- Dummy User -----
-#     user = {
-#         "full_name": "Officer Rohan Sharma",
-#         "rank": "Constable",
-#         "badge_number": "45231",
-#         "station": "Sector 14 Police Station"
-#     }
-
-#     # ----- Dummy Current Duty -----
-#     current_duty = {
-#         "location": "City Center Road",
-#         "partner": {"name": "Officer Rahul Singh"},
-#         "start_time": "10:00 AM",
-#         "end_time": "4:00 PM",
-#         "status": "Active",
-#     }
-
-#     # ----- Dummy Duty History -----
-#     duty_history = [
-#         {
-#             "date": "18 Nov 2025",
-#             "location": "Highway 21",
-#             "partner": {"name": "Officer Meena"},
-#             "status": "Completed",
-#         },
-#         {
-#             "date": "17 Nov 2025",
-#             "location": "Bus Stand Sector 5",
-#             "partner": {"name": "Officer Arjun"},
-#             "status": "Completed",
-#         },
-#     ]
-
-#     # ----- Dummy Notifications -----
-#     notifications = [
-#         {"text": "New duty assigned for tomorrow at 8 AM."},
-#         {"text": "Your leave request from 15 Nov approved."},
-#     ]
-
-#     # ----- Dummy Attendance -----
-#     attendance = {
-#         "checkin": "09:55 AM",
-#         "checkout": None,  # Means pending
-#     }
-
-#     context = {
-#         "user": user,
-#         "current_duty": current_duty,
-#         "duty_history": duty_history,
-#         "notifications": notifications,
-#         "attendance": attendance,
-#     }
-#     return render(request, "user_panel/user_dashboard.html",current_duty)
-
-#-----Custom user Panel Views-----
+#----- Custom user Panel Views -----
 def user_base(request):
     return render(request, "user_panel/user_base.html")
 def user_assign_duty(request):
@@ -150,6 +86,25 @@ def user_profile(request):
     return render(request, "user_panel/user_profile.html")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#----- firebase push notification -----
 def showFirebaseJS(request):
     data='importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");' \
          'importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js"); ' \
