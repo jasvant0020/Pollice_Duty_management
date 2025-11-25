b0c785e3abe691564201c3a3cd4e8e46f9a7fdb0
jasvant0020 <jasvant0singh123@gmail.com>
2025-11-25 11:54:23 +0530

    refine code structure

--- project/urls.py
+++ project/urls.py
@@ -4,11 +4,13 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     # GD Munsi Panel Pages
     path('', views.dashboard, name='dashboard'),
     path('police/', views.police_list, name='police_list'),
     path('vvip/', views.vvip_list, name='vvip_list'),
     path('duty/assign/', views.assign_duty, name='assign_duty'),
+
      # Custom Admin Panel Pages
     path('admin_base/', views.admin_base, name='admin_base'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
@@ -17,12 +19,9 @@ urlpatterns = [
     path('manage/manage_users/', views.manage_users, name='manage_users'),
     path('manage/manage_police_categories/', views.manage_police_categories, name='manage_police_categories'),
     path('manage/manage_vvip_categories/', views.manage_vvip_categories, name='manage_vvip_categories'),
-    # path('admin-panel/manage-users/', views.manage_users, name='manage_users'),
-    # path('admin-panel/manage-police-categories/', views.manage_police_categories, name='manage_police_categories'),
-    # path('admin-panel/manage-vvip-categories/', views.manage_vvip_categories, name='manage_vvip_categories'), 
-    # path("dashboard/", views.user_dashboard, name="user_dashboard"),
+
     # Custom user Panel Pages
-    path("user_dashboard/", views.user_base, name="user_base"),
+    path("user_base/", views.user_base, name="user_base"),
     path("user_assign_duty/", views.user_assign_duty, name="user_assign_duty"),
     path("request_application_box/", views.request_application_box, name="request_application_box"),
     path("duty_history/", views.duty_history, name="duty_history"),
@@ -30,6 +29,6 @@ urlpatterns = [
     path("attendance_panel/", views.attendance_panel, name="attendance_panel"),
     path("user_profile/", views.user_profile, name="user_profile"),
 
-
+    # Firebase push notification 
     path('firebase-messaging-sw.js',views.showFirebaseJS,name="show_firebase_js"),
 ]
