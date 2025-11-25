bedf046b3e7a499b267535b6b1ee0fdf75398bd0
jasvant0020 <jasvant0singh123@gmail.com>
2025-11-25 17:41:23 +0530

    minor changes

--- app/views.py
+++ app/views.py
@@ -88,7 +88,7 @@ def user_profile(request):
 
 
 # views.py
-def login_view(request):
+def login(request):
     if request.method == "POST":
         email = request.POST.get("email")
         password = request.POST.get("password")
