377409a470afaeeb5b697f8a9ae7ac93651b78ae
jasvant0020 <jasvant0singh123@gmail.com>
2025-12-05 16:46:03 +0530

    reset class CustomUser(AbstractUser) in models

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,12 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import User
+
+# class CustomUserAdmin(UserAdmin):
+#     fieldsets = UserAdmin.fieldsets + (
+#         ('Role & Ownership', {
+#             'fields': ('role', 'created_by', 'admin_owner', 'gd_munsi_owner')
+#         }),
+#     )
+
+# admin.site.register(User, CustomUserAdmin)
--- app/models.py
+++ app/models.py
@@ -1,17 +1,46 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    # add extra fields if needed
-    pass
 
-class SecurityCategory(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    total_personnel = models.PositiveIntegerField()
-    personnel_by_rank = models.JSONField(default=dict, blank=True)  # e.g., {"SP": 2, "Addl SP": 3}
+    ROLE_CHOICES = [
+        ('developer', 'Developer'),
+        ('master_admin', 'Master Admin'),
+        ('super_admin', 'Super Admin'),
+        ('admin', 'Admin'),
+        ('gd_munsi', 'GD Munsi'),
+        ('field_staff', 'Field Staff'),
+    ]
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    created_by = models.ForeignKey(
+        'self', on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name="children"
+    )
+
+    admin_owner = models.ForeignKey(
+        'self', on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name="admin_users"
+    )
+
+    gd_munsi_owner = models.ForeignKey(
+        'self', on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name="gd_staffs"
+    )
 
     def __str__(self):
-        return self.name
+        return f"{self.username} ({self.role})"
+
+# class SecurityCategory(models.Model):
+#     name = models.CharField(max_length=100, unique=True)
+#     total_personnel = models.PositiveIntegerField()
+#     personnel_by_rank = models.JSONField(default=dict, blank=True)  # e.g., {"SP": 2, "Addl SP": 3}
+
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.name
