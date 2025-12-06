8d4badbdcccc64595aca5dab61aa38430c582fde
jasvant0020 <jasvant0singh123@gmail.com>
2025-12-06 12:24:28 +0530

    login working based on role

--- project/urls.py
+++ project/urls.py
@@ -30,9 +30,11 @@ urlpatterns = [
     # -------------------------------------------------------
     # UPDATED: Added CRUD URLs for Manage Users functionality
     # -------------------------------------------------------
-    path('manage_users/add/', views.add_user, name='add_user'),           
+    path('add_user/', views.add_user, name='add_user'),           
     path('manage_users/edit/<int:user_id>/', views.edit_user, name='edit_user'),  
-    path('manage_users/delete/<int:user_id>/', views.delete_user, name='delete_user'),  
+    path('manage_users/delete/<int:user_id>/', views.delete_user, name='delete_user'), 
+    path('user_list/', views.user_list, name='user_list'),
+
     # -------------------------------------------------------
     # UPDATED: Added CRUD URLs for Manage Police Categories
     # -------------------------------------------------------
