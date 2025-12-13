c09868cb3035535245b85d440295ecd2da373d6d
jasvant0020 <jasvant0singh123@gmail.com>
2025-12-13 16:26:53 +0530

    making List Overview workable to show actual rankwise count

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(AbstractUser):
         null=True, blank=True
     )
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
-    rank = models.CharField(max_length=50, null=True, blank=True)
+    rank = models.CharField(max_length=150, null=True, blank=True)
     dob = models.DateField(null=True, blank=True)
 
     created_by = models.ForeignKey(
