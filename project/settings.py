06f2bd38ab2c4317d226eee546504d4b6b8756bb
jasvant0020 <jasvant0singh123@gmail.com>
2026-03-14 12:40:50 +0530

    setup for deployement

--- project/settings.py
+++ project/settings.py
@@ -68,10 +68,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 ASGI_APPLICATION = "project.asgi.application"
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
-    },
+CHANNEL_LAYERS= {
+"default": {
+"BACKEND":"channels_redis.core.RedisChannelLayer",
+"CONFIG": {
+"hosts": [("127.0.0.1",6379)],
+  },
+ }
 }
 
 
@@ -113,12 +116,24 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("DB_NAME"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("DB_NAME"),
+            "USER": os.environ.get("DB_USER"),
+            "PASSWORD": os.environ.get("DB_PASSWORD"),
+            "HOST": os.environ.get("DB_HOST"),
+            "PORT": os.environ.get("DB_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
@@ -159,11 +174,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-
-# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
